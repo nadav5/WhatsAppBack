3771f4f7dc19d686ec5c8bf0f41df5aea3e99036
nadav5 <nadavbar205@gmail.com>
2025-07-14 10:20:58 +0300

    Client Port Authority

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(new ValidationPipe());
 }
