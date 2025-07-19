2aaa7aa169a4785453083ecf2435a8e1e58b811d
nadav5 <nadavbar205@gmail.com>
2025-07-19 18:49:31 +0300

    online users

--- src/components/chat/chat.gateway.ts
+++ src/components/chat/chat.gateway.ts
@@ -20,24 +20,47 @@ export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer()
-  server: Server;
+  public server: Server;
+  private connectedUsers: { [userName: string]: string } = {}; // username => socketId
+
 
   afterInit(server: Server) {
     console.log('Socket server initialized');
   }
 
   handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+  const userName = client.handshake.query.userName as string;
 
-    client.on('join_chat', (chatId) => {
-      client.join(chatId);
-      console.log(`Client ${client.id} joined chat ${chatId}`);
-    });
+  if (userName) {
+    this.connectedUsers[userName] = client.id;
+    console.log(`User ${userName} connected with socket ${client.id}`);
+
+    this.server.emit('update_active_users', Object.keys(this.connectedUsers));
+  } else {
+    console.log(`Client connected without userName: ${client.id}`);
   }
+
+  client.on('join_chat', (chatId) => {
+    client.join(chatId);
+    console.log(`Client ${client.id} joined chat ${chatId}`);
+  });
+}
 
   handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+  const userName = Object.keys(this.connectedUsers).find(
+    (key) => this.connectedUsers[key] === client.id
+  );
+
+  if (userName) {
+    delete this.connectedUsers[userName];
+    console.log(`User ${userName} disconnected`);
+
+    this.server.emit('update_active_users', Object.keys(this.connectedUsers));
+  } else {
+    console.log(`Unknown client disconnected: ${client.id}`);
   }
+}
+
 
   @SubscribeMessage('send_message')
   handleMessage(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
